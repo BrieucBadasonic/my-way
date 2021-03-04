9d379f156d89dd18d12b412dfe474e2907a78758
Ishany Balder <ishany.balder@gmail.com>
2021-03-04 18:41:32 +0100

    map popup

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -48,15 +48,21 @@ const initMapbox = () => {
       });
 
       markers.forEach((marker) => {
+        var popupOffsets = {
+          'top': [0, 0],
+          'left': [-300],
+        };
+
         const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
         // Create a HTML element for your custom marker
         const element = document.createElement('div');
-        element.className = 'marker';
+        // element.className = 'marker';
         element.style.backgroundImage = `url('${marker.image_url}')`;
         element.style.backgroundSize = 'contain';
         element.style.width = '25px';
         element.style.height = '25px';
+
 
         // Pass the element as an argument to the new marker
         new mapboxgl.Marker(element)
