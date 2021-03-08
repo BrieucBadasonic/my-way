50a9f8404e3f751a6f18409e11f46c3f027d8ae8
Ishany Balder <ishany.balder@gmail.com>
2021-03-08 18:09:22 +0100

    added back and select buttons to the view page and designed view page

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,11 @@
 class GardensController < ApplicationController
   def show
+    if params[:trip].present?
+      @trip = Trip.find(params[:trip])
+      @segment = Segment.new
+
+    end
     @garden = Garden.find(params[:id])
+    @facilities = @garden.facilities
   end
 end
