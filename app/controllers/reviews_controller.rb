0a33525f43946391cd6bbafdd9939d122c80b582
Lisbeth Purrucker <lisbeth@cuba-buddy.de>
2021-03-08 12:45:50 +0100

    reviews and landing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+before_action :find_garden, only: [ :new, :create ]
+
+def new
+  @review = Review.new
+end
+
+def create
+  @review = Review.new(review_params)
+  @review.user = current_user
+  @review.garden = @garden
+
+  if @review.save
+    redirect_to garden_path(@garden, anchor: "review-#{@review.id}")
+  else
+    render 'garden/show'
+  end
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:description, :rating,
+    :garden_id)
+end
+
+def find_garden
+  garden = Garden.find(params[:garden_id])
+end
+
 end
