12e8a6cdd8726cc1f91255d61b8abd0a8a3bb7e8
Lisbeth Purrucker <lisbeth@cuba-buddy.de>
2021-03-10 21:10:17 +0100

    garden show

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,13 +4,4 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :description, :rating, presence: true
-
-
-  # def average_rating(garden)
-  #   where(garden_id: garden).average(:rating)
-  # end
-
-  # def average_rating
-  #   self.class.average(:rating).where(id: garden.id)
-  # end
 end
