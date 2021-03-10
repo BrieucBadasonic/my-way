5d56fcd6cda45295b29875484802f8b93d6b1af6
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-03-10 13:47:45 +0100

    metttta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   def default_url_options
   { host: ENV["www.myway.bike"] || "localhost:3000" }
-end
+  end
 end
